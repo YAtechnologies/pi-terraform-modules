c224c902a8bcbc6255f745fa515ce8dcbf6e5a04
Walid Ziouche <01walid@users.noreply.github.com>
2025-02-11 23:40:08 +0100

    fix(gcp): convert list to set for gcs-lb-domain project service resource

--- modules/gcp/gcs-lb-domain/main.tf
+++ modules/gcp/gcs-lb-domain/main.tf
@@ -2,7 +2,7 @@
 
 # Enable the services needed for this module
 resource "google_project_service" "services" {
-  for_each           = local.services
+  for_each           = toset(local.services)
   project            = data.google_client_config.current.project
   service            = each.value
   disable_on_destroy = false

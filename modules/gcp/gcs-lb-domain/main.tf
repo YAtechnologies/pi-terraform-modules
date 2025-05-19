de39237183db612f8efe0000307c8a1afab6722d
Walid Ziouche <01walid@users.noreply.github.com>
2025-05-19 12:23:55 +0100

    remove ignore changes for SSL certificate

--- modules/gcp/gcs-lb-domain/main.tf
+++ modules/gcp/gcs-lb-domain/main.tf
@@ -30,10 +30,6 @@ resource "google_compute_managed_ssl_certificate" "ssl_cert" {
   managed {
     domains = var.domains
   }
-
-  lifecycle {
-    ignore_changes = [expire_time]
-  }
 }
 
 # GCP URL MAP

641f2c439206bed1b4c46dcab6df1c9c587570de
Walid Ziouche <01walid@users.noreply.github.com>
2025-05-19 12:06:28 +0100

    Ignore expire_time for SSL policies

--- modules/gcp/gcs-lb-domain/main.tf
+++ modules/gcp/gcs-lb-domain/main.tf
@@ -30,6 +30,10 @@ resource "google_compute_managed_ssl_certificate" "ssl_cert" {
   managed {
     domains = var.domains
   }
+
+  lifecycle {
+    ignore_changes = ["expire_time"]
+  }
 }
 
 # GCP URL MAP
